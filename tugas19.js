6bad40667ff190cbf5d99bc6eb6cb9f5a4ba3b60
sinar96 <sinar96>
2020-01-24 20:08:54 +0800

    repeat js

---
+++ tugas19.js
@@ -0,0 +1,18 @@
+function Tugas1() {
+    var str1 = "Semangat...";
+    console.log(str1.repeat(5));
+}
+Tugas1();
+
+function Tugas2() {
+    var str2 = "*****";
+    console.log(str2);
+    console.log(str2.repeat(5));
+    console.log(str2.repeat(9));
+    console.log(str2.repeat(16));
+    console.log(str2.repeat(9));
+    console.log(str2.repeat(5));
+    console.log(str2);
+    
+}
+Tugas2();
